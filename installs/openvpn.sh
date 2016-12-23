d2a542036fe93787f87f1cedf1bbb9ca7240afb9
Socrates P <socrat3z@gmail.com>
2016-12-23 22:53:36 +0200

    Update openvpn.sh

--- installs/openvpn.sh
+++ installs/openvpn.sh
@@ -2,3 +2,15 @@
 # Source = https://github.com/Nyr/openvpn-install
 
 wget --no-check-certificate https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh
+
+
+# -------- autostart on debian/ubuntu
+# http://askubuntu.com/questions/464264/starting-openvpn-client-automatically-at-boot
+
+autoconnect 
+put file to /etc/openvpn/client.conf <- client.ovpn
+
+# ------------ settings for reconnection 
+resolv-retry infinite
+keepalive 10 120
+
