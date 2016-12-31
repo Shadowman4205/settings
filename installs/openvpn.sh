26880405ba038b9bd059d6589c3a50c799fb1697
Socrates P <socrat3z@gmail.com>
2016-12-31 13:57:51 +0200

    Update openvpn.sh

--- installs/openvpn.sh
+++ installs/openvpn.sh
@@ -6,6 +6,7 @@ wget --no-check-certificate https://git.io/vpn -O openvpn-install.sh && bash ope
 
 # -------- autostart on debian/ubuntu
 # http://askubuntu.com/questions/464264/starting-openvpn-client-automatically-at-boot
+# caution: on scaleway the ip ranges can conflict - so change from 10.8.0.0 to 10.9.0.0 in server.conf
 
 autoconnect 
 put file to /etc/openvpn/client.conf <- client.ovpn
@@ -13,4 +14,3 @@ put file to /etc/openvpn/client.conf <- client.ovpn
 # ------------ settings for reconnection 
 resolv-retry infinite
 keepalive 10 120
-
