83b6d1f37ab7cf7782b21592894ecad3f80cabf9
Socrates P <socrat3z@gmail.com>
2017-01-08 15:41:56 +0200

    Update openvpn.sh

--- installs/openvpn.sh
+++ installs/openvpn.sh
@@ -3,6 +3,15 @@
 
 wget --no-check-certificate https://git.io/vpn -O openvpn-install.sh && bash openvpn-install.sh
 
+# Setting up iptables on ubuntu
+sudo nano /etc/rc.local
+# at the very top of the file before all the # comments insert:
+
+iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+iptables -I INPUT -p udp --dport 1194 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to SERVERIPADDRESS
+#(remember to replace SERVERIPADDRESS with your IP)
 
 # -------- autostart on debian/ubuntu
 # http://askubuntu.com/questions/464264/starting-openvpn-client-automatically-at-boot
