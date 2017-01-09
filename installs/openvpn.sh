48ca30dd2d19916020d75c2947f56159070248a8
Socrates P <socrat3z@gmail.com>
2017-01-09 21:46:46 +0200

    Update openvpn.sh

--- installs/openvpn.sh
+++ installs/openvpn.sh
@@ -7,6 +7,9 @@ wget --no-check-certificate https://git.io/vpn -O openvpn-install.sh && bash ope
 sudo nano /etc/rc.local
 # at the very top of the file before all the # comments insert:
 
+iptables -t nat -A POSTROUTING -s 192.168.6.0/24 -o eth0 -j MASQUERADE # works on SCALEWAY
+
+# or 
 iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
 iptables -I INPUT -p udp --dport 1194 -j ACCEPT
